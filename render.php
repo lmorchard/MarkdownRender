47f028d581546a80983341c9ceabac561b1c3436
Leslie Michael Orchard <l.m.orchard@pobox.com>
2011-03-06 23:21:35 -0500

    Trying out Disqus comments

--- render.php
+++ render.php
@@ -57,6 +57,24 @@ if (!is_file($file)) {
     <body>
         <article>
             <section><?=$text?></section>
+            <section class="comments">
+                <div id="disqus_thread"></div>
+                <script type="text/javascript">
+                    var disqus_shortname = '<?= htmlspecialchars($disqus_shortname) ?>';
+
+                    // var disqus_identifier = 'unique_dynamic_id_1234';
+                    // var disqus_url = 'http://example.com/permalink-to-page.html';
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+            </section>
         </article>
     </body>
 </html>
